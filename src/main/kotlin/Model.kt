b5b64a09d3793e7406fe647dde26d271a424d08a
fmoorenoo <fermontemore@gmail.com>
2024-11-07 13:32:58 +0000

    Variable 'users' en Model.kt

---
+++ src/main/kotlin/Model.kt
@@ -0,0 +1,11 @@
+data class User(val name: String, val image: String){
+}
+
+val users = listOf<User>(
+    User("Persona1 Apellido1", "user1.jpeg"),
+    User("Persona2 Apellido2", "user2.jpeg"),
+    User("Persona3 Apellido3", "user3.jpeg"),
+    User("Persona4 Apellido4", "user4.jpeg"),
+    User("Persona5 Apellido5", "user5.jpeg"),
+    User("Persona6 Apellido6", "user6.jpeg")
+)
