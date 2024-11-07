e54afb24952959faeaa4d4da6276cb4b0f6feda3
fmoorenoo <fermontemore@gmail.com>
2024-11-07 13:26:51 +0000

    Main y Model creados

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,18 +11,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
-@Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
