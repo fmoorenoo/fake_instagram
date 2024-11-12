1849f8f494f287753eeba8af3ca0803fdb024e50
fmoorenoo <fermontemore@gmail.com>
2024-11-12 13:29:40 +0000

    Sugerencias añadidas y posts de prueba en Model.kt

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -2,6 +2,9 @@ data class User(val name: String, val image: String){}
 
 data class Post(val image: String, val img_autor: String, val autor: String, val description: String){}
 
+data class Suggestion(val name: String, val image: String){}
+
+
 val users = listOf<User>(
     User("unai_perez", "user1.jpg"),
     User("amaro_morales", "user2.jpg"),
@@ -12,5 +15,15 @@ val users = listOf<User>(
 )
 
 val posts = listOf<Post>(
-    Post("post1.jpeg", "autor1.jpg", "Messi", "El MVC está sobrevalorado."),
+    Post("post1.jpg", "autor1.jpg", "Messi", "Odio a Cristiano Ronaldo, pero odio más AndroidStudio. #kotlin #futbol"),
+    Post("post1.jpg", "autor1.jpg", "Messi", "Odio a Cristiano Ronaldo, pero odio más AndroidStudio. #kotlin #futbol"),
+    Post("post1.jpg", "autor1.jpg", "Messi", "Odio a Cristiano Ronaldo, pero odio más AndroidStudio. #kotlin #futbol"),
+    Post("post1.jpg", "autor1.jpg", "Messi", "Odio a Cristiano Ronaldo, pero odio más AndroidStudio. #kotlin #futbol"),
+)
+
+val suggestions = listOf<Suggestion>(
+    Suggestion("unai_perez", "user1.jpg"),
+    Suggestion("amaro_morales", "user2.jpg"),
+    Suggestion("simon_gonzález", "user3.jpg"),
+    Suggestion("iriemdaluz", "user4.jpg")
 )
