05c32d9b60570158622eecf95f0f29a1d17bcdaf
fmoorenoo <fermontemore@gmail.com>
2024-11-12 08:57:46 +0000

    Nombres de usuarios cambiados

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -2,10 +2,10 @@ data class User(val name: String, val image: String){
 }
 
 val users = listOf<User>(
-    User("Persona1 Apellido1", "user1.jpeg"),
-    User("Persona2 Apellido2", "user2.jpeg"),
-    User("Persona3 Apellido3", "user3.jpeg"),
-    User("Persona4 Apellido4", "user4.jpeg"),
-    User("Persona5 Apellido5", "user5.jpeg"),
-    User("Persona6 Apellido6", "user6.jpeg")
+    User("User1", "user1.jpeg"),
+    User("User2", "user2.jpeg"),
+    User("User3", "user3.jpeg"),
+    User("User4", "user4.jpeg"),
+    User("User5", "user5.jpeg"),
+    User("User6", "user6.jpeg")
 )
