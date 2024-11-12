a1444256091d2e889655096eee32f6584ed11eb1
fmoorenoo <fermontemore@gmail.com>
2024-11-12 12:49:13 +0000

    Nuevo tamaño de la ventana

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 @Composable
 fun App() {
@@ -106,9 +108,9 @@ fun App() {
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Instagram"
+        title = "Instagram",
+        state = rememberWindowState(size = DpSize(1000.dp, 1200.dp))
     ) {
         App()
     }
 }
-
