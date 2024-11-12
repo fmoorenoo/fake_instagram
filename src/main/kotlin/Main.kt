21544355745419f2d1be16dbc3c973e1cfabfad5
fmoorenoo <fermontemore@gmail.com>
2024-11-12 13:15:59 +0000

    Sugerencias empezadas. En fila con las pubcaciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,11 +18,12 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun App() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Instagram", fontSize = 30.sp)
+    Column(modifier = Modifier.padding(vertical = 20.dp, horizontal = 40.dp)) {
+        Text(text = "Instagram", fontSize = 30.sp, fontWeight = FontWeight.Bold)
 
         Text(text = "Historias", fontSize = 18.sp, modifier = Modifier.padding(top = 8.dp))
         Row(
@@ -31,7 +32,7 @@ fun App() {
         ) {
             users.forEach { user ->
                 var username = user.name
-                if ((user.name).length > 10) {
+                if (username.length > 10) {
                     username = username.substring(0, 8) + "..."
                 }
                 Column(
@@ -56,59 +57,72 @@ fun App() {
             }
         }
 
-        Text(text = "Publicaciones", fontSize = 18.sp, modifier = Modifier.padding(top = 16.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Publicaciones", fontSize = 18.sp, modifier = Modifier.padding(top = 16.dp))
 
-        LazyColumn(modifier = Modifier.padding(top = 8.dp).width(280.dp)) {
-            items(posts) { post ->
-                Card(
-                    shape = RoundedCornerShape(12.dp),
-                    backgroundColor = Color(0xFFF5F5F5),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 10.dp)
-                        .width(280.dp),
-                    elevation = 6.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        Image(
+                LazyColumn(modifier = Modifier.padding(top = 8.dp).width(280.dp)) {
+                    items(posts) { post ->
+                        Card(
+                            shape = RoundedCornerShape(12.dp),
+                            backgroundColor = Color(0xFFF5F5F5),
                             modifier = Modifier
-                                .size(200.dp)
-                                .clip(RoundedCornerShape(8.dp))
-                                .align(Alignment.CenterHorizontally),
-                            painter = painterResource(resourcePath = "post_image/" + post.image),
-                            contentDescription = "Foto publicada"
-                        )
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.padding(vertical = 10.dp)
+                                .fillMaxWidth()
+                                .padding(vertical = 10.dp)
+                                .width(280.dp),
+                            elevation = 6.dp
                         ) {
-                            Image(
-                                modifier = Modifier
-                                    .size(45.dp)
-                                    .clip(CircleShape),
-                                painter = painterResource(resourcePath = "post_image/" + post.img_autor),
-                                contentDescription = "Foto del autor"
-                            )
-                            Text(text = post.autor, modifier = Modifier.padding(start = 8.dp))
+                            Column(
+                                modifier = Modifier.padding(16.dp)
+                            ) {
+                                Image(
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                        .align(Alignment.CenterHorizontally),
+                                    painter = painterResource(resourcePath = "post_image/" + post.image),
+                                    contentDescription = "Foto publicada"
+                                )
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(vertical = 10.dp)
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(45.dp)
+                                            .clip(CircleShape),
+                                        painter = painterResource(resourcePath = "post_image/" + post.img_autor),
+                                        contentDescription = "Foto del autor"
+                                    )
+                                    Text(text = post.autor, modifier = Modifier.padding(start = 8.dp))
+                                }
+                                Text(
+                                    text = post.description,
+                                    modifier = Modifier.padding(top = 4.dp)
+                                )
+                            }
                         }
-                        Text(
-                            text = post.description,
-                            modifier = Modifier.padding(top = 4.dp)
-                        )
                     }
                 }
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                Text(text = "Sugerencias", fontSize = 18.sp, modifier = Modifier.padding(bottom = 8.dp))
             }
         }
     }
 }
 
+
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Instagram",
-        state = rememberWindowState(size = DpSize(1000.dp, 1200.dp))
+        state = rememberWindowState(size = DpSize(800.dp, 1200.dp))
     ) {
         App()
     }
