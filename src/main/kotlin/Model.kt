a80a8b4dac5abe8fff01996400719b60a9046a0a
fmoorenoo <fermontemore@gmail.com>
2024-11-12 14:55:55 +0000

    Datos de las sugerencias de fotos

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -10,8 +10,8 @@ val users = listOf<User>(
     User("amaro_morales", "user2.jpg"),
     User("simon_gonzález", "user3.jpg"),
     User("iriemdaluz", "user4.jpg"),
-    User("fer", "user5.jpg"),
-    User("Messi", "user6.jpg")
+    User("feer.mm_", "user5.jpg"),
+    User("fabio_waszshowitsf", "user6.jpg")
 )
 
 val posts = listOf<Post>(
@@ -21,9 +21,18 @@ val posts = listOf<Post>(
     Post("post1.jpg", "autor1.jpg", "messi", "Odio a Cristiano Ronaldo, pero odio más AndroidStudio. #kotlin #futbol"),
 )
 
-val suggestions = listOf<Suggestion>(
+val users_suggestions = listOf<Suggestion>(
     Suggestion("cristiano", "user1.jpg"),
     Suggestion("thegrefg", "user2.jpg"),
     Suggestion("fortnitepro_2009", "user3.jpg"),
     Suggestion("shrek_official", "user4.jpg")
+)
+
+val photo_suggestions = listOf<Suggestion>(
+    Suggestion("", "photo1.jpg"),
+    Suggestion("", "photo2.jpg"),
+    Suggestion("", "photo3.jpg"),
+    Suggestion("", "photo4.jpg"),
+    Suggestion("", "photo5.jpg"),
+    Suggestion("", "photo6.jpg"),
 )
